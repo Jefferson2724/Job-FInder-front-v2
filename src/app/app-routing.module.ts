a930c2c75d25cfd4c9550d9c4745806eb4b2da77
jefferson.nascimento <jefferson.nascimento@fastsolucoes.com.br>
2022-05-17 21:40:18 -0300

    [JB-2] Iniciando tela de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './main/home/home.component';
+import { LoginComponent } from './main/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'home/:id', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './main/home/home.component';
+import { LoginComponent } from './main/login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+      HomeComponent,
+      LoginComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
