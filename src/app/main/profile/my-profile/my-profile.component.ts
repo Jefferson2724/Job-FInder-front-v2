234083cac47522a3cc54138f46bf40a691c9c5a3
jefferson.nascimento <jefferson.nascimento@fastsolucoes.com.br>
2022-07-22 01:51:42 -0300

    Ajustando fluxo para exibicao de informacoes no meu perfil

--- src/app/main/profile/my-profile/my-profile.component.ts
+++ src/app/main/profile/my-profile/my-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-my-profile',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-profile.component.css']
 })
 export class MyProfileComponent implements OnInit {
-  infoUser: any = "empresa";
-  constructor() { }
+  isStudent: boolean;
+  idUser: string;
+  infoUserDTO: any;
+  
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private authenticationService: AuthenticationService,
+  ) { }
 
   ngOnInit() {
+      this.idUser = this.activatedRoute.snapshot.params['id'];
+      this.infoUser();
+  }
+
+  infoUser(){
+    this.authenticationService.getUserById(this.idUser).subscribe(
+      response => {
+          if(!response) {
+              return;
+          }else if( response == "veio nada man") {
+              this.infoCompany();
+              return;
+          }
+          
+          this.isStudent = true;
+          this.infoUserDTO = response;
+      }
+    );
+  }
+
+  infoCompany() {
+    this.authenticationService.getCompanyById(this.idUser).subscribe(
+      response => {
+          if(!response) {
+              return;
+          }
+
+          this.isStudent = false;
+          this.infoUserDTO = response;
+      }
+    );
   }
 
 }
